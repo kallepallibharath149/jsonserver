44373b03a140fcf3b796c80d1aae9b5dceb5c94d
kallepallibharath149 <31881419+kallepallibharath149@users.noreply.github.com>
2020-07-05 22:51:48 +0530

    changes

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ console.log('calling this for all')
 var StatusNotes=[
   {"status": "Altered",
     "note": "05/08/2020",
-    "date":"6/5/2020, 10:16:53 PM"},
+    "date":"08/5/2020, 10:16:53 PM"},
     {"status": "Altered",
     "note": "05/08/2020",
     "date":"5/5/2020, 10:16:53 PM"},
